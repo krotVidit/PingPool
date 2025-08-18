6663465f932789e1e29a3f0793557b0249e10386
krotVidit <backendkid@mail.ru>
2025-08-18 19:09:01 +0300

    [BASE] Базовый шаблон

--- main.go
+++ main.go
@@ -1,4 +1,56 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
 func main() {
+	const workerCount = 3
+	wg := sync.WaitGroup{}
+	in := make(chan string)
+	out := make(chan string)
+
+	urls := []string{
+		"https://ya.ru/",
+		"https://www.google.com/",
+		"https://vk.com",
+		"https://netology.ru",
+	}
+
+	go func() {
+		for _, url := range urls {
+			in <- url
+		}
+		close(in)
+	}()
+
+	for i := 0; i < workerCount; i++ {
+		wg.Add(1)
+		go worker(in, out, &wg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	for resutl := range out {
+		fmt.Println(resutl)
+	}
 }
+
+func worker(in <-chan string, out chan<- string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for url := range in {
+		resp, err := http.DefaultClient.Get(url)
+		if err != nil {
+			out <- fmt.Sprintf("Error: %s  url: %s", err, url)
+			continue
+		}
+		out <- fmt.Sprintf("OK %s status: %s", url, resp.Status)
+		resp.Body.Close()
+	}
+}
+
---
+++ workerpool/worker.go
@@ -0,0 +1,5 @@
+// Package workerpool будет лежать паттерни workerpool
+package workerpool
+
+func worker(id int, in <-chan string, out <-chan string) {
+}
